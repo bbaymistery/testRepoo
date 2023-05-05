d3cc9830698246584d26a960e7b926e8b3d54a93
Elgun <elgun.ezmemmedov@mail.ru>
2023-05-05 19:22:10 +0400

    's'

---
+++ src/components/widgets/TaxiDeals/index.js
@@ -0,0 +1,130 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import env from '../../../resources/env'
+import PointsModal from '../../elements/PointsModal'
+
+import styles from "./styles.module.scss"
+import TaxiDealViewContent from './TaxiDealViewContent'
+import useRipple from '../../../hooks/useRipple'
+import { useRef } from 'react'
+const tabsBttons = [
+    {
+        name: "Heathrow Taxi Deals",
+        id: 1,
+        dealsName: "heathrow"
+    },
+    {
+        name: "Gatwick Taxi Deals",
+        id: 2,
+        dealsName: "gatwick"
+
+    },
+    {
+        name: "Stansted Taxi Deals",
+        id: 3,
+        dealsName: "stansted"
+
+    },
+    {
+        name: "Luton Taxi Deals",
+        id: 4,
+        dealsName: "luton"
+
+    },
+    {
+        name: "City Airport",
+        id: 5,
+        dealsName: "city"
+
+    }
+]
+//showTabs=>they come from here > heathrow-airport-transfer
+//isLinknameComponent comes driom [..linkname]
+const TaxiDeals = (props) => {
+
+
+    let { showTabs = true, bggray = false, islinknamecomponent = false } = props
+    const dispatch = useDispatch()
+    const state = useSelector(state => state.pickUpDropOffActions)
+    let { params: { direction, language, pointsModalStatus, hasTaxiDeals = "heathrow" } } = state
+
+    const [tabs, setTabs] = useState(0)
+    const [taxiPoints, setTaxiPoints] = useState([])
+    const [dealsName, setdealsName] = useState(hasTaxiDeals)
+    const refs = tabsBttons.map(() => useRef(null));
+    const ripples = refs.map((ref) => useRipple(ref));
+
+
+    const fecthPoints = async (params = {}) => {
+        let { language, dealsNameProp = hasTaxiDeals } = params
+        let channelId = state.reservations[0].reservationDetails.channelId
+        let url = `${env.apiDomain}/api/v1/taxi-deals/list?points=${dealsNameProp}&language=${language}&channelId=${channelId}`;
+        let response = await fetch(url);
+        let { data, status } = await response.json();
+        if (status === 200) setTaxiPoints(data.destinations)
+    }
+    const tabsHandler = async (params = {}) => {
+        let { index, dealsNameProp } = params
+        setTabs(index)
+        setdealsName(dealsNameProp)
+        fecthPoints({ dealsNameProp, language })
+    }
+
+
+    const setModal = () => {
+        dispatch({ type: "SET_POINTS_MODAL", data: { trueOrFalse: true } })
+        document.body.style.overflow = "hidden";
+    }
+
+    useEffect(() => {
+        fecthPoints({ dealsNameProp: hasTaxiDeals, language })
+    }, [language, hasTaxiDeals,])
+
+
+    return (
+        <>
+            {
+                <div className={`${styles.taxideals} ${direction} ${islinknamecomponent ? styles.islinkname : ""} page `} bggray={String(bggray)} >
+                    {pointsModalStatus && <PointsModal points={taxiPoints} title={`${dealsName} Transfer Deals`} />}
+                    <div className={`${styles.taxideals_section} page_section`}>
+                        <div className={`${styles.taxideals_section_container} page_section_container`}>
+
+                            {taxiPoints.length > 1 ?
+                                <div className={styles.title}>
+                                    {hasTaxiDeals === 'dover' || hasTaxiDeals === 'southampton' || hasTaxiDeals === 'portsmouth' || hasTaxiDeals === 'harwich' ? <h1>{hasTaxiDeals} Cruise Port</h1> : <h1>{hasTaxiDeals} Taxi Deals</h1>}
+                                    {islinknamecomponent ? "" : <p>All Inclusive Price !</p>}
+                                </div> : <></>}
+                            {showTabs ?
+                                <div className={`${styles.tabs} ${styles.btn_div}`}>
+                                    {tabsBttons.map((btn, index) => {
+                                        return (<button
+                                            onClick={() => tabsHandler({ index, dealsNameProp: btn.dealsName })}
+                                            className={`${tabs === index ? styles.active : ""} btn`}
+                                            key={index}
+                                            ref={refs[index]}
+                                        >
+                                            <div className="ripple-wrapper">{ripples[index]}</div>
+                                            {btn.name}
+                                        </button>)
+                                    }
+                                    )}
+                                </div>
+                                : <></>}
+                            {taxiPoints.length > 1 ? <TaxiDealViewContent islinknamecomponent={islinknamecomponent} points={taxiPoints} dealsName={showTabs ? dealsName : hasTaxiDeals} /> : <></>}
+                            {taxiPoints.length > 1 ? <div className={styles.btn_div}>
+                                <button className='btn' onClick={() => { setModal() }}>
+                                    View All
+                                    <i className="fa-solid fa-arrow-right"></i>
+                                </button>
+                            </div> : <></>}
+                        </div>
+                    </div>
+                </div >}
+        </>
+    )
+}
+
+
+
+export default TaxiDeals
---
+++ src/store/accountRegisterReducer/index.js
@@ -0,0 +1,80 @@
+import SET_APPLICANT_FOR_CREADIT_DETAILS from "./SET_APPLICANT_FOR_CREADIT_DETAILS";
+import SET_APPROXIMATE_DATA from "./SET_APPROXIMATE_DATA";
+import SET_CONTACT_DETAILS from "./SET_CONTACT_DETAILS";
+/*
+    accountRegisterDatas: {
+        applicantForCreditAccount: {
+            companyName: "companyName",
+            natureOfBusiness: "natureOfBusiness",
+            companyFax: "companyFax",
+            postCode: "postCode",
+            address: "address",
+            registrationNo: "registrationNo",
+            companyTel: "companyTel"
+        },
+        contactDetails: {
+            contactName: "test",
+            jobTitle: "test",
+            department: "test",
+            email: "test@mail.ru",
+            telephoneNo: "telephoneNo"
+        },
+        approximateSpend: "122"
+    }
+
+      accountRegisterDatas: {
+        applicantForCreditAccount: {
+            companyName: "",
+            natureOfBusiness: "",
+            companyFax: "",
+            postCode: "",
+            address: "",
+            registrationNo: "",
+            companyTel: ""
+        },
+        contactDetails: {
+            contactName: "",
+            jobTitle: "",
+            department: "",
+            email: "",
+            telephoneNo: ""
+        },
+        approximateSpend: ""
+    }
+*/
+const INITIAL_STATE = {
+    accountRegisterDatas: {
+        applicantForCreditAccount: {
+            companyName: "companyName",
+            natureOfBusiness: "natureOfBusiness",
+            postCode: "postCode",
+            address: "address",
+            registrationNo: "registrationNo",
+            companyTel: "companyTel"
+        },
+        contactDetails: {
+            contactName: "test",
+            jobTitle: "test",
+            department: "test",
+            email: "test@mail.ru",
+            telephoneNo: "telephoneNo"
+        },
+        approximateSpend: "122"
+    }
+}
+
+export const accountRegisterActions = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CONTACT_DETAILS': {
+            return SET_CONTACT_DETAILS({ state, action })
+        }
+        case 'SET_APPROXIMATE_DATA': {
+            return SET_APPROXIMATE_DATA({ state, action })
+        }
+        case 'SET_APPLICANT_FOR_CREADIT_DETAILS': {
+            return SET_APPLICANT_FOR_CREADIT_DETAILS({ state, action })
+        }
+        default:
+            return state;
+    }
+}
