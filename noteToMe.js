e6f92a83957fdf429d32d6847d7fdd50f5e667b7
Elgun <elgun.ezmemmedov@mail.ru>
2023-07-03 13:30:43 +0400

    's'

--- noteToMe.js
+++
@@ -1,3 +0,0 @@
-
-//mesela .../tr   de homa tiklayinca       direk engce ye gecir ki gecmemelidi  > yani apltest/tr   olmalidi
-//mobile de language degisende asagi acilan dropdown getmir o yuzden dropdownu clickable elenmeliyik
