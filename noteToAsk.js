dd6cab63d3d26f126c5af058ec501a86e406031e
Elgun <elgun.ezmemmedov@mail.ru>
2023-07-04 11:56:07 +0400

    'aplAirtest'

---
+++ noteToAsk.js
@@ -0,0 +1,29 @@
+
+
+//!manageMyBooking.tml  > which id i will include 2 or 3
+//!should i change language inside passenger details
+//!when we change language some of social links changes Are we gonna make it based on backend api ?
+//!( 486 customer reviews )   <can we make api to get reviews from there Cause it changes every time
+//!  Aplcars.com >>>bizi niye airportpickupsa yonlendirir?\
+//taxi dealsden devam ederken odeme asamasina geder pay by cash deyende error verir Laith dediki duzeldecem onu sorus
+//[linkname]=>deki title metaga taglar
+//Sitemap olucakmi
+//xml taxi prices
+//special offers
+//about us
+//contact us > meta tags
+//http://localhost:3500/heathrow-airport-transfer  >meta tags
+//"http://localhost:3500/heathrow-airport-transfer" >resimler taxideals
+//https://api.london-tech.com/api/v1/taxi-deals/list?points=harwick&language=tr&channelId=2 >>>i cnt make request to harwich Why? (cruise portda linklerin axrncisina tikliyanda)
+//https://www.airport-pickups-london.com/Heathrow/Heathrow-Taxi-Prices.asp   her linke uygun content gelicekmidir?
+//http://localhost:3500/heathrow/heathrow-to-dover-taxi   >486 review olani apiden nece alacagig?
+// https://www.airport-pickups-london.com/Cruise-taxi   Bu sayfa olucakmi?
+// 
+/*
+suggestions
+{
+    "value" : "lhr",
+"language": "tr"
+}   why we dont have translatedAddress
+*/
+
