45da4087f774fd2b8bd70b424680eb241909c1d5
Elgun <elgun.ezmemmedov@mail.ru>
2023-08-18 11:35:35 +0400

    's'

---
+++ src/constants/taxideals/index.js
@@ -0,0 +1,71 @@
+export const taxidealsImages = [
+    {
+        id: 1,
+        image: "/images/hgtto/toOxford.jpg",
+    },
+    {
+        id: 2,
+        image: "/images/hgtto/toBrimingham.jpg",
+    },
+    {
+        id: 3,
+        image: "/images/hgtto/toCambridge1.jpg",
+    },
+    {
+        id: 4,
+        image: "/images/hgtto/toBath.jpg",
+    },
+    {
+        id: 5,
+        image: "/images/hgtto/toBristol.jpg",
+    },
+    {
+        id: 6,
+        image: "/images/hgtto/toBrighton.jpg",
+    },
+    {
+        id: 7,
+        image: "/images/hgtto/toNorwich.jpg",
+    },
+    {
+        id: 8,
+        image: "/images/hgtto/toCanterburry.jpg",
+    },
+
+]
+
+export const cruisePortimages = [
+    {
+        id: 1,
+        image: "/images/hgtto/heathrowto.jpg",
+    },
+    {
+        id: 2,
+        image: "/images/hgtto/gatwickto.jpg",
+    },
+    {
+        id: 3,
+        image: "/images/hgtto/stansetto.jpg",
+    },
+    {
+        id: 4,
+        image: "/images/hgtto/lutonto.jpg",
+    },
+    {
+        id: 5,
+        image: "/images/hgtto/cityairport.jpg",
+    },
+    {
+        id: 6,
+        image: "/images/hgtto/toheathrow.jpg",
+    },
+    {
+        id: 7,
+        image: "/images/hgtto/gatwickto.jpg",
+    },
+    {
+        id: 8,
+        image: "/images/hgtto/stansetto.jpg",
+    },
+
+]
