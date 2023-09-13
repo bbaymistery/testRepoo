e860abdf4fca0491468ab0bb66844f876e1d8108
Elgun <elgun.ezmemmedov@mail.ru>
2023-09-13 15:20:18 +0400

    's'

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,28 +4,12 @@ import CarsSlider from "../components/widgets/CarsSlider";
 import TaxiDeals from "../components/widgets/TaxiDeals";
 import Tours from "./tours";
 import { useEffect, useState } from "react";
+import { createWrapper } from "next-redux-wrapper";
 
 
-export default function Home() {
-  const [countryCode, setCountryCode] = useState(''); // Default value is 'gb'
+export default function Home(props) {
+console.log(props);
 
-  useEffect(() => {
-    // This function fetches the country code from our API route
-    const fetchCountry = async () => {
-      try {
-        const res = await fetch('/api/getCountry');
-        const data = await res.json();
-        console.log(data);
-        console.log(`  Country code is: ${data.country}`);
-
-      } catch (error) {
-        console.error("Failed to fetch the country:", error);
-      }
-    };
-
-
-    fetchCountry();
-  }, []);
   return (
     <GlobalLayout footerbggray={true}>
       <Hero />
@@ -39,24 +23,32 @@ export default function Home() {
   )
 }
 
-// const makestore = () => store;
-// const wrapper = createWrapper(makestore);
+const makestore = () => store;
+const wrapper = createWrapper(makestore);
 
-// export const getServerSideProps = wrapper.getServerSideProps(store => async ({ req, res, ...etc }) => {
-//   const paymentUrl = `${env.apiDomain}/api/v1/payment-types`;
-//   const appDataUrl = `${env.apiDomain}/app/en`;
-//   const urls = [paymentUrl, appDataUrl];
+export const getServerSideProps = wrapper.getServerSideProps(store => async ({ req, res, ...etc }) => {
+  // const paymentUrl = `${env.apiDomain}/api/v1/payment-types`;
+  // const appDataUrl = `${env.apiDomain}/app/en`;
+  // const urls = [paymentUrl, appDataUrl];
 
-//   let response = await Promise.all(urls.map(async url => {
-//     let resp = await fetch(url);
-//     return resp.json();
-//   }));
+  // let response = await Promise.all(urls.map(async url => {
+  //   let resp = await fetch(url);
+  //   return resp.json();
+  // }));
 
-//   const appData = response[1];
-//   const paymentTypes = response[0].data;
-//   // Dispatch values to Redux store
-//   store.dispatch({ type: "GET_APP_DATA", data: { appData: appData, paymentTypes: paymentTypes, }, });
+  // const appData = response[1];
+  // const paymentTypes = response[0].data;
+  // // Dispatch values to Redux store
+  // store.dispatch({ type: "GET_APP_DATA", data: { appData: appData, paymentTypes: paymentTypes, }, });
+  const forwarded = req.headers["x-forwarded-for"]
+  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+  console.log(ip);
 
-// });
+  return {
+    props: {
+      ip,
+    },
+  }
+});
 
 
