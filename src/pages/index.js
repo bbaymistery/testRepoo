a353fcdd32d5065dba46635fcb12dcf32f5362af
Elgun <elgun.ezmemmedov@mail.ru>
2023-09-13 15:35:45 +0400

    's'

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import TaxiDeals from "../components/widgets/TaxiDeals";
 import Tours from "./tours";
 import { useEffect, useState } from "react";
 import { createWrapper } from "next-redux-wrapper";
-
+import geoip from 'geoip-lite';
 
 export default function Home(props) {
-console.log(props);
+  console.log(`IP Address: ${props.ip}`);
+  console.log(`Country: ${props.country}`);
+
 
   return (
     <GlobalLayout footerbggray={true}>
@@ -40,13 +42,23 @@ export const getServerSideProps = wrapper.getServerSideProps(store => async ({ r
   // const paymentTypes = response[0].data;
   // // Dispatch values to Redux store
   // store.dispatch({ type: "GET_APP_DATA", data: { appData: appData, paymentTypes: paymentTypes, }, });
-  const forwarded = req.headers["x-forwarded-for"]
-  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
-  console.log(ip);
+  const forwarded = req.headers["x-forwarded-for"];
+  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+
+  // Use geoip-lite to lookup country based on IP address
+  const geo = geoip.lookup(ip);
+
+  let country = "gb";  // default country
+  if (geo && geo.country) {
+    country = geo.country.toLowerCase();
+  }
+
+  console.log(`IP: ${ip}, Country: ${country}`);
 
   return {
     props: {
       ip,
+      country,
     },
   }
 });
