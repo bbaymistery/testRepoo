5bf9ff139b9778289cb2af89a2cb5a53fb6bbccd
Elgun <elgun.ezmemmedov@mail.ru>
2023-09-14 16:34:31 +0400

    's'

--- rudimentary/index.js
+++ rudimentary/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import styles from "./styles.module.scss"
-
+ 
 const indexPage = (props) => {
     let { bggray } = props
     const state = useSelector(state => state.pickUpDropOffActions)
