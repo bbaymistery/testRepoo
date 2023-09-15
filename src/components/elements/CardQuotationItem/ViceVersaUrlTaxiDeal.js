e13b005616e3bb5ea4662233a0089ce56ae5a7e4
Elgun <elgun.ezmemmedov@mail.ru>
2023-09-15 12:43:03 +0400

    's'

---
+++ src/components/elements/CardQuotationItem/ViceVersaUrlTaxiDeal.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from "./styles.module.scss"
+const ViceVersaUrlTaxiDeal = ({ previousUrl, returnPageTitle ,returnHeadTitle ,returnPathname }) => {
+
+    return (
+        <>
+            {/* {previousUrl.split("/").length === 4 ?
+                <p className={styles.viceversa}>
+                    <a href={returnPathname} title={"Return Page Title"}>
+                        Return Page Title_ transfer quote, please click here
+                    </a>
+                </p> : <></>} */}
+            {
+                <p className={styles.viceversa}>
+                    <a href={returnPathname} title={returnPageTitle}>
+                        {returnHeadTitle}
+                    </a>
+                </p>}
+        </>
+    )
+}
+
+export default ViceVersaUrlTaxiDeal
---
+++ src/constants/seaportTransfers/index.js
@@ -0,0 +1,31 @@
+export const seaport = [
+  {
+    title: "Southampton Cruise Terminal",
+    linkUrl: "/southampton",
+    imgUrl: "/images/seaport/sct.webp",
+    id: 1,
+
+  },
+  {
+    id: 2,
+
+    title: "Dover Cruise Terminal",
+    linkUrl: "/dovercruise",
+    imgUrl: "/images/seaport/dct.webp",
+  },
+  {
+    id: 3,
+
+    title: "Portsmouth Cruise Port",
+    linkUrl: "/portsmouth",
+    imgUrl: "/images/seaport/pcb.webp",
+  },
+
+  {
+    id: 4,
+    linkUrl: "/harwich",
+    title: "Harwich Cruise Port",
+    imgUrl: "/images/seaport/hcp.webp",
+  },
+];
+
