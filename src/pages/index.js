d044e2c8cbf3c639b9c319e4951e57dcd94dd802
Elgun <elgun.ezmemmedov@mail.ru>
2023-12-04 17:10:58 +0400

    's'

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ import dynamic from 'next/dynamic'
 import { useEffect, useState } from "react";
 const TaxiDeals = dynamic(() => import('../components/widgets/TaxiDeals'), { loading: () => <div>Loading...</div> });
 const CarsSlider = dynamic(() => import('../components/widgets/CarsSlider'),);
-// const SeaportTransfers = dynamic(() => import('../components/widgets/SeaportTransfers'),);
+const SeaportTransfers = dynamic(() => import('../components/widgets/SeaportTransfers'),);
 const Tours = dynamic(() => import('./tours'),);
-import SeaportTransfers from '../components/widgets/SeaportTransfers'
-export default function Home() {
+
+export default function Home(props) {
   const [hasScrolled, setHasScrolled] = useState(false);
   const handleScroll = () => {
     if (!hasScrolled) {
@@ -25,7 +25,7 @@ export default function Home() {
     <GlobalLayout footerbggray={true}>
       <Hero />
       <TaxiDeals />
-      <SeaportTransfers bggray={true} />
+      {hasScrolled && <SeaportTransfers bggray={true} />}
       {hasScrolled && <Tours insideGlobalLayout={false} />}
       {hasScrolled && <CarsSlider bggray={true} />}
       <Testimonials bggray={false} />
