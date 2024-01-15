708c5d7b1b7e1f9a332653a7fc8debbc6c4954ef
Elgun <elgun.ezmemmedov@mail.ru>
2024-01-15 14:52:44 +0400

    's'

--- src/components/elements/QuotationResultsTaxiDeal/index.js
+++ src/components/elements/QuotationResultsTaxiDeal/index.js
@@ -74,5 +74,10 @@ const QuotationResultsTaxiDeal = (props) => {
     </GlobalLayout>
     )
 }
-
+export async function getServerSideProps({ req, res }) {
+    res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59')
+    return {
+        props: {},
+    }
+}
 export default QuotationResultsTaxiDeal
