4c2c214e6a57fa07eeccfa62a24b12ed3018bab2
Elgun <elgun.ezmemmedov@mail.ru>
2024-01-15 14:46:13 +0400

    's'

--- src/components/elements/QuotationResultsTaxiDeal/index.js
+++ src/components/elements/QuotationResultsTaxiDeal/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./styles.module.scss"
 import GlobalLayout from '../../layouts/GlobalLayout'
 import { useSelector } from 'react-redux'
 import CardQuotationItemTaxiDeal from '../CardQuotationItemTaxiDeal'
 import dynamic from 'next/dynamic'
 import { useWindowSize } from '../../../hooks/useWindowSize'
+import { useEffect } from 'react'
+import Loading from '../alert/Loading'
 const QuotationResultsTaxiDealLeftPart = dynamic(() => import('../QuotationResultsTaxiDealLeftPart'));
 const QuotationResultsTaxiDeal = (props) => {
     //these props comes from ...pathname
@@ -28,42 +30,48 @@ const QuotationResultsTaxiDeal = (props) => {
     const objectDetailss = appData?.pointTypeCategories?.reduce((obj, item) => ({ ...obj, [item.id]: JSON.parse(item.objectDetails), }), {});
     const size = useWindowSize();
     const { width } = size;
-    return (
-        <GlobalLayout keywords={keywordsTaxiDeal} title={headTitle} description={descriptionTaxiDeal} footerbggray={true}  >
-            <div className={`${styles.quotation} page`}>
-                <div className={`${styles.quotation_section} page_section`}>
-                    <div className={`${styles.quotation_section_container} page_section_container`}>
-                        {reservations.map((obj, index) => {
-                            return (
-                                <div key={index} style={{ marginBottom: `${index === 0 ? "4rem" : ""}` }}>
-                                    <div className={`${direction} ${styles.main_container_taxideal}`}>
-                                        {width > 1280 ? <QuotationResultsTaxiDealLeftPart /> : <></>}
-                                        {/* //*Card item of results */}
-                                        <CardQuotationItemTaxiDeal
-                                            index={index}
-                                            distance={quotations[index].distance}
-                                            duration={quotations[index].duration}
-                                            selectedQuotation={reservations[index]?.quotation}
-                                            quotationOptions={quotations[index].quotationOptions}//
-                                            headTitle={headTitle}
-                                            previousUrl={previousUrl}
-                                            returnPathname={returnPathname}
-                                            pageTitle={pageTitle}
-                                            pageContent={pageContent}
-                                            returnHeadTitle={returnHeadTitle}
-                                            returnPageTitle={returnPageTitle}
-                                            objectDetailss={objectDetailss}
-                                        />
+    const [loadAlert, setLoadAlert] = useState(true)
 
-                                    </div>
+    useEffect(() => {
+        if (quotations[0]?.quotationOptions?.length > 0) {
+            setLoadAlert(false)
+        }
+    }, [quotations])
+    return (loadAlert ? <Loading /> : <GlobalLayout keywords={keywordsTaxiDeal} title={headTitle} description={descriptionTaxiDeal} footerbggray={true} pathnameProp={true}  >
+        <div className={`${styles.quotation} page`}>
+            <div className={`${styles.quotation_section} page_section`}>
+                <div className={`${styles.quotation_section_container} page_section_container`}>
+                    {reservations.map((obj, index) => {
+                        return (
+                            <div key={index} style={{ marginBottom: `${index === 0 ? "4rem" : ""}` }}>
+                                <div className={`${direction} ${styles.main_container_taxideal}`}>
+                                    {width > 1280 ? <QuotationResultsTaxiDealLeftPart /> : <></>}
+                                    {/* //*Card item of results */}
+                                    <CardQuotationItemTaxiDeal
+                                        index={index}
+                                        distance={quotations[index].distance}
+                                        duration={quotations[index].duration}
+                                        selectedQuotation={reservations[index]?.quotation}
+                                        quotationOptions={quotations[index].quotationOptions}//
+                                        headTitle={headTitle}
+                                        previousUrl={previousUrl}
+                                        returnPathname={returnPathname}
+                                        pageTitle={pageTitle}
+                                        pageContent={pageContent}
+                                        returnHeadTitle={returnHeadTitle}
+                                        returnPageTitle={returnPageTitle}
+                                        objectDetailss={objectDetailss}
+                                    />
+
                                 </div>
-                            )
-                        })
-                        }
-                    </div>
+                            </div>
+                        )
+                    })
+                    }
                 </div>
             </div>
-        </GlobalLayout>
+        </div>
+    </GlobalLayout>
     )
 }
 
