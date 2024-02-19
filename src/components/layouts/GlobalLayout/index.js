440cbc90a548b743cbee0c65e01d70ae28eb5068
bbaymistery <elgun.ezmemmedov@mail.ru>
2024-02-19 02:37:09 -0800

    s

--- src/components/layouts/GlobalLayout/index.js
+++ src/components/layouts/GlobalLayout/index.js
@@ -13,63 +13,63 @@ const structuredSchema = {
   "telephone": "+44 208 688 7744",
   "priceRange": "35",
   "address": {
-      "@type": "PostalAddress",
-      "postalCode": "UB7 9HJ",
-      "streetAddress": "APL Office, Cherry Lane",
-      "addressCountry": "GB",
-      "addressLocality": "West Drayton"
+    "@type": "PostalAddress",
+    "postalCode": "UB7 9HJ",
+    "streetAddress": "APL Office, Cherry Lane",
+    "addressCountry": "GB",
+    "addressLocality": "West Drayton"
   },
   "aggregateRating": {
-      "@type": "AggregateRating",
-      "ratingValue": "4.94",
-      "bestRating": "5",
-      "worstRating": "1",
-      "ratingCount": "1772",
-      "reviewCount": "1772"
+    "@type": "AggregateRating",
+    "ratingValue": "4.94",
+    "bestRating": "5",
+    "worstRating": "1",
+    "ratingCount": "1772",
+    "reviewCount": "1772"
   },
   "review": {
-      "@type": "Review",
-      "name": "Airport Pickups London",
-      "reviewBody": "Driver was very professional  on time and good company ",
-      "reviewRating": {
-          "@type": "Rating",
-          "ratingValue": "5",
-          "bestRating": "5",
-          "worstRating": "1"
-      },
-      "datePublished": "2024-02-14",
-      "author": {
-          "@type": "Person",
-          "name": "Mrs. Elizabeth Williams"
-      },
-      "publisher": {
-          "@type": "Organization",
-          "name": "Reviews.io"
-      }
+    "@type": "Review",
+    "name": "Airport Pickups London",
+    "reviewBody": "Driver was very professional  on time and good company ",
+    "reviewRating": {
+      "@type": "Rating",
+      "ratingValue": "5",
+      "bestRating": "5",
+      "worstRating": "1"
+    },
+    "datePublished": "2024-02-14",
+    "author": {
+      "@type": "Person",
+      "name": "Mrs. Elizabeth Williams"
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "Reviews.io"
+    }
   },
   "geo": {
-      "@type": "GeoCoordinates",
-      "latitude": 51.49759087451855,
-      "longitude": -0.455451293899482
+    "@type": "GeoCoordinates",
+    "latitude": 51.49759087451855,
+    "longitude": -0.455451293899482
   },
   "openingHoursSpecification": {
-      "@type": "OpeningHoursSpecification",
-      "dayOfWeek": [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday"
-      ],
-      "opens": "00:00",
-      "closes": "23:59"
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ],
+    "opens": "00:00",
+    "closes": "23:59"
   },
   "sameAs": [
-      "https://www.facebook.com/AirportPickupsLondon",
-      "https://twitter.com/Airport_Pickups",
-      "https://www.youtube.com/c/Airport-pickups-london"
+    "https://www.facebook.com/AirportPickupsLondon",
+    "https://twitter.com/Airport_Pickups",
+    "https://www.youtube.com/c/Airport-pickups-london"
   ]
   // Include other properties as needed
 };
@@ -79,7 +79,8 @@ const GlobalLayout = (
     description = "Airport Pickups London offers  London airport taxi, transfers and airport shuttle services. We cover Heathrow, Gatwick, Stansted, Luton and City Airport and UK Cruise port.",
     keywords = "London airport transfers ,Heathrow pickups,Gatwick pickups,airport pickups,Heathrow transfer,Gatwick transfer ,Stansted transfer ,Luton transfer ,Heathrow airport transfer ,Gatwick airport transfer.",
     footerbggray = false,
-    isVisible = false
+    isVisible = false,
+    forHomePage = true
   }
 ) => {
 
@@ -102,14 +103,15 @@ const GlobalLayout = (
         {(router.pathname === "/") || (router.pathname === '/[...pathname]') ? <link rel="stylesheet" href="/fontawesome/css/all.min.css" /> : <link rel="stylesheet" href="/fontawesomeHomePage/css/all.min.css" />}
         {/* {router.pathname === "/" ? <></> : <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500&display=swap" />} */}
         <script type="application/ld+json">
-            {JSON.stringify(structuredSchema, null, 2)}
+          {JSON.stringify(structuredSchema, null, 2)}
         </script>
       </Head>
-      <TopHeader />
+      {forHomePage ? <TopHeader /> : <></>}
       <main>
         {children}
       </main>
-      {((isVisible && router.pathname === '/[...pathname]') || router.pathname !== '/[...pathname]') && (<Footer bggray={footerbggray} />)}
+      {((isVisible && router.pathname === '/[...pathname]') || (router.pathname !== '/[...pathname]' && forHomePage)) && (<Footer bggray={footerbggray} />)}
+
 
     </>
   )
--- src/components/widgets/Testimonials/index.js
+++ src/components/widgets/Testimonials/index.js
@@ -1,46 +1,37 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import styles from "./styles.module.scss"
-import { useWindowSize } from '../../../hooks/useWindowSize'
-import dynamic from 'next/dynamic';
+// import { useWindowSize } from '../../../hooks/useWindowSize'
+// import dynamic from 'next/dynamic';
 import usePageContentHook from '../../../hooks/usePageContentHook';
-const ShowContent = dynamic(() => import('../TestimonialContent/ShowContent'),);
+import GlobalLayout from '../../layouts/GlobalLayout';
+// const ShowContent = dynamic(() => import('../TestimonialContent/ShowContent'),);
 
 const Testimonials = (props) => {
     let { bggray } = props
     const state = useSelector(state => state.pickUpDropOffActions)
     let { params: { direction, language } } = state
 
-    const [showText, setshowText] = useState(false)
-    let size = useWindowSize()
-    let { width } = size
+    // const [showText, setshowText] = useState(false)
+    // let size = useWindowSize()
+    // let { width } = size
 
-    const { description, pageContent, shortDescription } = usePageContentHook("", language);
+    const { metaTitle, keywords, description, pageContent, pageTitle, shortDescription } = usePageContentHook("", language);
 
     const createMarkup = (htmlString) => { return { __html: htmlString } };
+    // const { appData } = useSelector(state => state.initialReducer)
 
 
     return (
-
-        <div className={`${styles.testimonials} ${direction} page`} bggray={String(bggray)} style={{ backgroundColor: `${String(bggray) === "true" ? "#f5f5f5" : "white"}` }}>
-            <div className={`${styles.testimonials_section} page_section`}>
-                <div className={`${styles.testimonials_section_container} page_section_container`}>
-                    <h1>{description}</h1>
-                    <p className={styles.sub_desc}>
-                        {shortDescription}
-                        <button  onClick={() => setshowText(!showText)}>
-                            {showText ? "Hide" : "Show More"}
-                        </button>
-                    </p>
-
-                    {width > 768 && showText ?
-                        <ShowContent >
-                            <div className={styles.dangerous_div} dangerouslySetInnerHTML={createMarkup(pageContent)} />
-                        </ShowContent> : <></>}
-                
+        <GlobalLayout keywords={keywords} title={metaTitle} description={description} forHomePage={false}>
+            <div className={`${styles.testimonials} ${direction} page`} bggray={String(bggray)} style={{ backgroundColor: `${String(bggray) === "true" ? "#f5f5f5" : "white"}` }}>
+                <div className={`${styles.testimonials_section} page_section`}>
+                    <div className={`${styles.testimonials_section_container} page_section_container`}>
+                        <div className={styles.dangerous_div} dangerouslySetInnerHTML={createMarkup(pageContent)} />
+                    </div>
                 </div>
             </div>
-        </div>
+        </GlobalLayout>
 
     )
 }
