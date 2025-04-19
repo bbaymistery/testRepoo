93e895154d9c34e903a28bc8728a48cbaada7e7f
bbaymistery <elgun.ezmemmedov@mail.ru>
2025-04-19 17:36:18 +0400

    serverjs amke tru

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const next = require("next");
 const { urls } = require('./src/resources/urls');
 
 // Set development or production mode
-const dev =  false; // process.env.NODE_ENV !== "production";
+const dev =  true; // process.env.NODE_ENV !== "production";
 const app = next({ dev, dir: '.', conf: { distDir: 'build' } }); // Custom build directory
 const handle = app.getRequestHandler(); // Default request handler
 
