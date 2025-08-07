b7735361fef2aec648397eff00565d3f31dcaa00
bbaymistery <elgun.ezmemmedov@mail.ru>
2025-08-07 13:32:00 +0400

    S

--- src/components/elements/TaxiDealsContents/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from "./styles.module.scss";
-const TaxiDealsContents = (props) => {
-        const { pageContent, isVisible } = props;
-        return (isVisible ? <div className={styles.description}>  <div dangerouslySetInnerHTML={{ __html: pageContent }}></div></div> : <></>);
-};
-
-export default TaxiDealsContents;
