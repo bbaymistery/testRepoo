3cbe757e47f9c48ce4c9f6fca978ca32d6d08a03
bbaymistery <elgun.ezmemmedov@mail.ru>
2026-03-16 13:01:15 +0400

    S

--- note.jsx
+++
@@ -1,5 +0,0 @@
-//!Card sandboxmu degilmi ona bak 
-//!getconfig file bak    const isDevelopment = false; olmali
-
-// c7V!Jam2
-// sb-yalzi46900575@personal.example.com
--- src/pages/reservations-document/ReservationObject/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ReservationObject = () => {
-  return (
-    <div>ReservationObject</div>
-  )
-}
-
-export default ReservationObject
